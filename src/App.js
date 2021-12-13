3464ef9e02c70e550f07dbf0e93c33479c510bd9
DGebrie <81825713+DGebrie@users.noreply.github.com>
2021-12-13 15:07:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "twosmov",
+      caption: "You asked for my hustle, I gave you my heart. RIP24",
+      imageUrl:
+        "https://cdn.dribbble.com/users/2289238/screenshots/9780542/kobe-bryant-rip-1.jpg",
+    },
+    {
+      username: "twosmov",
+      caption: "You asked for my hustle, I gave you my heart. RIP24",
+      imageUrl:
+        "https://cdn.dribbble.com/users/2289238/screenshots/9780542/kobe-bryant-rip-1.jpg",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram logo"
+        />
+      </div>
+      <h1> INSTAGRAM CLONE</h1>
+
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
+      {/* <Post
+        username="twosmov"
+        caption="You asked for my hustle, I gave you my heart.
+        RIP24"
+        imageUrl="https://cdn.dribbble.com/users/2289238/screenshots/9780542/kobe-bryant-rip-1.jpg"
+      />
+      <Post
+        username="mebratu2"
+        caption="goat x goat"
+        imageUrl="https://static.independent.co.uk/2021/04/16/07/newFile.jpg?width=982&height=726&auto=webp&quality=75"
+      />
+      <Post
+        username="jordan23air"
+        caption="Flu Game: '97 NBA Finals Game 6 @Utah Jazz"
+        imageUrl="https://theundefeated.com/wp-content/uploads/2017/06/gettyimages-86342542-1.jpg?w=700"
+      /> */}
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Post.css";
+import { Avatar } from "@mui/material";
+
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="Dejen"
+          // alt={username}
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+
+      <img className="post__image" src={imageUrl} alt="kobe bryant" />
+      {/* image */}
+      <h4 className="post__text">
+        <strong>{username} </strong>
+        {caption}
+      </h4>
+      {/* username + caption */}
+      {/* comments */}
+    </div>
+  );
+}
+
+export default Post;
